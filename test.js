43e9c3b5b9c3f1dc6a3a41f8e91890fbcfc9acec
Sebastien Bro <brosebastien@gmail.com>
2018-04-06 11:57:42 +0200

    config(project): Replace assert by expect

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+      expect([1,2,3].indexOf(4)).to.equal(-1);
     });
   });
 });
